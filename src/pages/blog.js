96e1cb384d78b55b0ce138ed3b3a70454e6bac4f
pavanraghuwanshi <pavanraghuwanshi77@gmail.com>
2024-04-23 22:29:48 +0530

    initial commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,7 +4,7 @@ const Blogs = () => {
   return (
     <div>
         <div className='bg-gray-900  flex mt-0  justify-center align-middle p-28' >
-            <h1 className='text-purple-700 font-bold text-xl bg-white p-4 rounded-md'>Currently Blogs Have Not Created By Me.</h1>
+            <h1 className='text-purple-700 font-bold text-xl bg-white p-4 rounded-md'>Currently Blogs Have Not Created By me.</h1>
         </div>
     </div>
   )
